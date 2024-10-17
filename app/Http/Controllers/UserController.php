ae1a9784881abffb112cbac08d238c2672a2d2dc
devina <devinacitra17@gmail.com>
2024-10-17 13:33:38 +0700

    perunahan 6

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -22,7 +22,7 @@ class UserController extends Controller
     public function index()
     {
         $data = [
-        'title' => 'List User',
+        'title' => 'Create User',
         'users' => $this->userModel->getUser(),
         ];
 
@@ -44,18 +44,41 @@ class UserController extends Controller
      $kelasModel = new Kelas();
      $kelas = $kelasModel->getKelas();
      $data = [
-     'kelas' => $kelas,
+        'title' => 'Create User',
+        'kelas' => $kelas,
      ];
      return view('create_user', $data);
     }
 
     public function store(Request $request)
     {
-     $this->userModel->create([
-     'nama' => $request->input('nama'),
-     'npm' => $request->input('npm'),
-     'kelas_id' => $request->input('kelas_id'),
-     ]);
-     return redirect()->to('/user');
+        $request->validate([
+            'nama' => 'required|string|max:255',
+            'npm' => 'required|string|max:255',
+            'kelas_id' => 'required|integer',
+            'foto' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+        if ($request->hasFile('foto')) {
+            $foto = $request->file('foto');
+            $filename = time() . '_' . $foto->getClientOriginalName();
+            $fotoPath = $foto->move(('upload/img'), $filename);
+        } else {
+            $fotoPath = null;
+        }
+        $this->userModel->create([
+            'nama' => $request->input('nama'),
+            'npm' => $request->input('npm'),
+            'kelas_id' => $request->input('kelas_id'),
+            'foto' => $filename,
+        ]);
+        return redirect()->to('/user')->with('success', 'User berhasil ditambahkan');
+    }
+    public function show($id){
+        $user = $this->userModel->getUser($id);
+        $data = [
+            'title' => 'Profile',
+            'user' => $user,
+        ];
+        return view('profile', $data);    
     }
 }
